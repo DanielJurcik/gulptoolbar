14a7815f40d64221ae646258102666d3ec0cf16c
Daniel Jurčík <dano.jurcik@gmail.com>
2022-03-27 14:22:37 +0200

    Added sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,50 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { HelloWorldPanel } from './HelloWorldPanel';
+import { SidebarProvider } from './SidebarProvider';
+import * as cp from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
+
+	//Sidebar
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider("gulp-sidebar", sidebarProvider)
+	);
+
 	HelloWorldPanel.createOrShow(context.extensionUri);
 	
+	const execShell = (cmd: string) =>
+    new Promise<string>((resolve, reject) => {
+        cp.exec(cmd, (err, out) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(out);
+        });
+    });
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gulptoolbar.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('gulptoolbar.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GULP Toolbar!');
+
+		const currentDir = await execShell('ls');
+		vscode.window.showInformationMessage(currentDir);
+
 		HelloWorldPanel.createOrShow(context.extensionUri);
+
+		const cwd = '.';
+		const command = `node -e "console.log('hi!');"`;
+
+		vscode.window.showInformationMessage(await execShell(command));
+		//vscode.window.showInformationMessage(await execShell("git pull origin master"));
+		
+
 	});
 
 	context.subscriptions.push(disposable);
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let count = 0;
+    let text = '';
+</script>
+
+<h1>{count}</h1>
+<button on:click={() => {count++;}}>{count}</button>
+
+<h2>Sync tool:</h2>
+<input type="text" bind:value={text}>
+<p>Sync tag :{text}</p>
